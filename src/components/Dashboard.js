c094f5958db9ee74c45cda9e98440c61c32a1b68
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-08-14 13:37:52 +0200

    Add Tabs to switch between questions types

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,24 +3,44 @@ import { connect } from 'react-redux';
 import Question from './Question';
 
 class Dashboard extends Component {
+  state = {
+    tabName: 'unansweredQuestions',
+  };
+
+  handleSwitchTabs = (tabName) => {
+    this.setState(() => ({
+      tabName,
+    }));
+
+    console.log('')
+  };
+
   render() {
     const { unAnsweredQuestionsIds, answeredQuestionsIds } = this.props;
     return (
       <div>
         <h1>Dashboard</h1>
 
-        <h2>Unanswered Questions</h2>
-        <ul>
-          {unAnsweredQuestionsIds.map((questionId) => (
-            <li key={questionId}> {<Question id={questionId} />}</li>
-          ))}
-        </ul>
-        <h2>Answered Questions</h2>
-        <ul>
-          {answeredQuestionsIds.map((questionId) => (
-            <li key={questionId}>{<Question id={questionId} />}</li>
-          ))}
-        </ul>
+        <button onClick={() => this.handleSwitchTabs('unansweredQuestions')}>
+          Unanswered Questions
+        </button>
+        <button onClick={() => this.handleSwitchTabs('answeredQuestions')}>
+          Answered Questions
+        </button>
+
+        {this.state.tabName === 'unansweredQuestions' ? (
+          <ul>
+            {unAnsweredQuestionsIds.map((questionId) => (
+              <li key={questionId}> {<Question id={questionId} />}</li>
+            ))}
+          </ul>
+        ) : (
+          <ul>
+            {answeredQuestionsIds.map((questionId) => (
+              <li key={questionId}>{<Question id={questionId} />}</li>
+            ))}
+          </ul>
+        )}
       </div>
     );
   }
